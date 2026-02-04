f8865e17143706df7a4cb8ca9332a3ab5ce28f5a
Christobal <christofferthorsnes@gmail.com>
2026-02-04 08:01:47 +0100

    La til øvefilene mine

---
+++ Øving/python/Variabler.py
@@ -0,0 +1,30 @@
+# Oppgave 1 - grunneleggende variabler
+
+x = 10 # X har verdien 10.
+y = "Johan" # Y har verdien "Johan".
+print(x) # Skriver ut verdien av x
+print(y) # Skriver ut verdien av y.
+
+# Oppgave 2 - endre variabler
+
+x = 5 # Endrer verdien av x til 5.
+y = "Ola" # Endrer verdien av y til "Ola".
+print(x) # Skriver ut den nye verdien av x.
+print(y) # Skriver ut den nye verdien av y.
+
+# Oppgave 3 - bruke variabler i beregninger
+
+a = 10
+b = 5
+c = 8
+
+sum = a + b + c # Beregner summen av a, b og c.
+print("Summen av a, b og c er:", sum) # Skriver ut summen av a, b og c.
+
+# Oppgave 4 - kombinere tekst og variabler
+
+x = "Hei jeg er: " 
+y = 10 # Alder
+
+print(x + str(y)) # Skriver ut teksten kombinert med alderen.
+
---
+++ Øving/python/loops/while_loop.py
@@ -0,0 +1,9 @@
+i = 0
+
+while i < 5:
+    i = i + 1
+    print(i)
+    
+
+while i < 5:
+    print('Hello, world')
